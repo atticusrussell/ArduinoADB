623748e5406a37200de616c01e823e99c1da39ab
Atticus Russell <atticusrussell@gmail.com>
2022-10-10 12:22:55 -0400

    added start/rough outline of script to flash Arduino to normal mode

---
+++ flashSerial.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "This script will flash your Arduino to serial (normal) mode so that sketches can be uploaded to it."
+echo "Put your Arduino into DFU mode by shorting the two pins nearest to the reset button (they will be on the 6-pin header nearest to the reset button)."
+read -n 1 -s -r -p "Press any key to continue and reflash once pin shorted"
+printf "\nErasing ATMega16U2 flash using dfu-programmer...\n"
+dfu-programmer atmega16u2 erase --force
+printf "Flashing Arduino-keyboard-0.3.hex using dfu-programmer...\n"
+dfu-programmer atmega16u2 flash Arduino-keyboard-0.3.hex
+printf "Resetting ATMega16U2 using dfu-programmer...\n"
+dfu-programmer atmega16u2 reset
+printf "Unplug and reconnect Arduino USB cable.\n"
+read -n 1 -s -r -p "Press any key to continue once Arduino reconnected"
+printf "Your Arduino should now be in serial (normal) mode and be able to be reprogrammed."
